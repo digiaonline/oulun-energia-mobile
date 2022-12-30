5719d8a2df335ac54441da9f51bd7a331a372445
Jani Koponen <jani.koponen@digia.com>
2022-12-30 18:22:18 +0200

    Change app bar size and add navigation bar theme

--- lib/views/theme/default_theme.dart
+++ lib/views/theme/default_theme.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 ThemeData defaultTheme = ThemeData(
-    appBarTheme: const AppBarTheme(
-        iconTheme: IconThemeData(size: 48, color: appBarIconColor),
-        toolbarHeight: 60,
-        color: appBarBackgroundColor));
+  appBarTheme: const AppBarTheme(
+    iconTheme: IconThemeData(size: 20, color: appBarIconColor),
+    toolbarHeight: 60,
+    backgroundColor: appBarBackgroundColor,
+    foregroundColor: Colors.black,
+  ),
+  navigationBarTheme: const NavigationBarThemeData(
+    indicatorColor: Color(0xFFF1F0F4),
+  ),
+);
 
 const Color appBarIconColor = Colors.black;
 const Color appBarBackgroundColor = Colors.white;
--- lib/views/utils/appbar.dart
+++ lib/views/utils/appbar.dart
@@ -4,12 +4,23 @@ import 'package:oulun_energia_mobile/views/login/login_view.dart';
 AppBar buildMainAppBar(BuildContext context,
     {String? title, Widget? titleWidget}) {
   return AppBar(
+    elevation: 1.0,
     centerTitle: true,
     title: titleWidget ?? (title != null ? Text(title) : null),
     leading: InkWell(
       onTap: () => Navigator.of(context).pushNamed(LoginView.routeName),
       child: const Icon(Icons.menu),
     ),
-    actions: const [Icon(Icons.face)],
+    actions: const [
+      Padding(
+        padding: EdgeInsets.only(
+          right: 20.0,
+        ),
+        child: Icon(
+          Icons.face,
+          size: 28.5,
+        ),
+      )
+    ],
   );
 }
