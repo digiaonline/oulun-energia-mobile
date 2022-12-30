52dceddc1fbeacb01a3adf936cb88da71ad49d3b
Jani Koponen <jani.koponen@digia.com>
2022-12-30 18:21:38 +0200

    Move enums to single file and add enums for Usages

---
+++ lib/core/enums.dart
@@ -0,0 +1,9 @@
+enum UsageInterval { interval, hour, day, week, month, year }
+
+enum UsageType { electric, districtHeating, missing }
+
+enum LoggedInStatus {
+  failed,
+  loggedIn,
+  loggedOut,
+}
