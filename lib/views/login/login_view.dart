c6693daccc8cd755ef10875617e5632f4225968f
Jani Koponen <jani.koponen@digia.com>
2023-01-30 12:54:05 +0200

    Refactor Login to use new CheckboxRow

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -10,6 +10,7 @@ import 'package:oulun_energia_mobile/views/terms/service_terms.dart';
 import 'package:oulun_energia_mobile/views/theme/default_theme.dart';
 import 'package:oulun_energia_mobile/views/theme/sizes.dart';
 import 'package:oulun_energia_mobile/views/utils/appbar.dart';
+import 'package:oulun_energia_mobile/views/utils/checkbox_row.dart';
 import 'package:oulun_energia_mobile/views/utils/input_box.dart';
 import 'package:oulun_energia_mobile/views/utils/widget_ext.dart';
 
@@ -39,62 +40,35 @@ class LoginView extends ConsumerWidget {
     var loginNotifier = ref.read(loginProvider.notifier);
     var theme = Theme.of(context);
     var locals = AppLocalizations.of(context)!;
-    return WillPopScope(
-      onWillPop: () async {
-        if (userAuth.loading) {
-          return false;
-        }
-
-        return true;
-      },
-      child: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            buildMainAppBar(context,
-                leading: null, foregroundColor: Colors.white),
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Container(
-                margin: Sizes.marginViewBorder,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        locals.loginViewLogin,
-                        style: theme.textTheme.headline2?.copyWith(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          InputBox(
-                              hintText: locals.loginViewUsernameHint,
-                              title: locals.loginViewUsername,
-                              keyboardType: TextInputType.text,
-                              multiline: false,
-                              controller: usernameController,
-                              textStyle:
-                                  defaultTheme.textTheme.bodyText2?.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                              ),
-                              border: const OutlineInputBorder(
-                                borderRadius: BorderRadius.zero,
-                                borderSide: BorderSide(
-                                  color: Colors.white,
-                                  width: 1,
-                                ),
-                              )),
-                          InputBox(
-                            hintText: locals.loginViewPasswordHint,
-                            title: locals.loginViewPassword,
-                            keyboardType: TextInputType.visiblePassword,
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          buildMainAppBar(context,
+              leading: null, foregroundColor: Colors.white),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Container(
+              margin: Sizes.marginViewBorder,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      locals.loginViewLogin,
+                      style: theme.textTheme.headline2?.copyWith(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        InputBox(
+                            hintText: locals.loginViewUsernameHint,
+                            title: locals.loginViewUsername,
+                            keyboardType: TextInputType.text,
                             multiline: false,
-                            obscureText: true,
-                            controller: passwordController,
+                            controller: usernameController,
                             textStyle:
                                 defaultTheme.textTheme.bodyText2?.copyWith(
                               color: Colors.white,
@@ -106,131 +80,131 @@ class LoginView extends ConsumerWidget {
                                 color: Colors.white,
                                 width: 1,
                               ),
-                            ),
+                            )),
+                        InputBox(
+                          hintText: locals.loginViewPasswordHint,
+                          title: locals.loginViewPassword,
+                          keyboardType: TextInputType.visiblePassword,
+                          multiline: false,
+                          obscureText: true,
+                          controller: passwordController,
+                          textStyle: defaultTheme.textTheme.bodyText2?.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
                           ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: userAuth.rememberSignIn,
-                                onChanged: (value) => loginNotifier
-                                    .rememberSignIn(value ?? false),
-                              ),
-                              const SizedBox(width: 5.0),
-                              Text(
-                                locals.loginViewRememberSignIn,
-                                style: defaultTheme.textTheme.bodyText2
-                                    ?.copyWith(color: Colors.white),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: Sizes.marginViewBorderSize,
-                          ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: userAuth.termsAccepted,
-                                onChanged: (value) =>
-                                    loginNotifier.acceptTerms(value ?? false),
-                              ),
-                              const SizedBox(width: 5.0),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text.rich(
-                                    TextSpan(
-                                      text: locals.loginViewTermsLinkPrefix,
-                                      style: defaultTheme.textTheme.bodyText2
-                                          ?.copyWith(color: Colors.white),
-                                      children: [
-                                        TextSpan(
-                                          text: locals.loginViewTermsLink,
-                                          style: defaultTheme
-                                              .textTheme.bodyText2
-                                              ?.copyWith(
-                                                  color: Colors.white,
-                                                  decoration:
-                                                      TextDecoration.underline),
-                                        ),
-                                      ],
-                                    ),
-                                  ).toClickable(
-                                      onTap: () => _openTermsLink(context)),
-                                  Text(
-                                    locals.loginViewPrivacyStatementLink,
-                                    style: defaultTheme.textTheme.bodyText2
-                                        ?.copyWith(
-                                            color: Colors.white,
-                                            decoration:
-                                                TextDecoration.underline),
-                                  ).toClickable(
-                                      onTap: () =>
-                                          _openPrivacyStatement(context)),
-                                ],
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: Sizes.marginViewBorderSize,
-                          ),
-                          userAuth.loading
-                              ? const Center(child: CircularProgressIndicator())
-                              : TextButton(
-                                  onPressed: userAuth.termsAccepted
-                                      ? () => _doLogin(
-                                          ref,
-                                          usernameController.text,
-                                          passwordController.text,
-                                          userAuth.rememberSignIn)
-                                      : null,
-                                  child: Text(
-                                    locals.loginViewLoginButton,
-                                    style: defaultTheme.textTheme.bodyText1
-                                        ?.copyWith(color: Colors.white),
-                                  ),
-                                ).toButton(enabled: userAuth.termsAccepted)
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                            locals.loginViewForgotPasswordLink,
-                            textAlign: TextAlign.center,
-                            style: theme.textTheme.bodyText2?.copyWith(
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.zero,
+                            borderSide: BorderSide(
                               color: Colors.white,
-                              decoration: TextDecoration.underline,
+                              width: 1,
                             ),
-                          ).toClickable(
-                              onTap: () => _openForgotPassword(context)),
-                          const SizedBox(
-                            height: Sizes.marginViewBorderSize,
                           ),
-                          Text.rich(
-                            textAlign: TextAlign.center,
-                            TextSpan(
-                              text: locals.loginViewRegisterLinkPrefix,
-                              style: defaultTheme.textTheme.bodyText2
-                                  ?.copyWith(color: Colors.white),
-                              children: [
+                        ),
+                        CheckboxRow(
+                          color: Colors.white,
+                          value: userAuth.rememberSignIn,
+                          onChanged: (value) =>
+                              loginNotifier.rememberSignIn(value ?? false),
+                          child: Text(
+                            locals.loginViewRememberSignIn,
+                            style: defaultTheme.textTheme.bodyText2
+                                ?.copyWith(color: Colors.white),
+                          ),
+                        ),
+                        CheckboxRow(
+                          color: Colors.white,
+                          value: userAuth.termsAccepted,
+                          onChanged: (value) =>
+                              loginNotifier.acceptTerms(value ?? false),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text.rich(
                                 TextSpan(
-                                  text: locals.loginViewRegisterLink,
+                                  text: locals.loginViewTermsLinkPrefix,
                                   style: defaultTheme.textTheme.bodyText2
-                                      ?.copyWith(
-                                          color: Colors.white,
-                                          decoration: TextDecoration.underline),
+                                      ?.copyWith(color: Colors.white),
+                                  children: [
+                                    TextSpan(
+                                      text: locals.loginViewTermsLink,
+                                      style: defaultTheme.textTheme.bodyText2
+                                          ?.copyWith(
+                                              color: Colors.white,
+                                              decoration:
+                                                  TextDecoration.underline),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ).toClickable(onTap: () => _openRegistering(context)),
-                        ],
-                      ),
-                    ]),
-              ),
+                              ).toClickable(
+                                  onTap: () => _openTermsLink(context)),
+                              Text(
+                                locals.loginViewPrivacyStatementLink,
+                                style: defaultTheme.textTheme.bodyText2
+                                    ?.copyWith(
+                                        color: Colors.white,
+                                        decoration: TextDecoration.underline),
+                              ).toClickable(
+                                  onTap: () => _openPrivacyStatement(context)),
+                            ],
+                          ),
+                        ),
+                        userAuth.loading
+                            ? const Center(child: CircularProgressIndicator())
+                            : TextButton(
+                                onPressed: userAuth.termsAccepted
+                                    ? () => _doLogin(
+                                        ref,
+                                        usernameController.text,
+                                        passwordController.text,
+                                        userAuth.rememberSignIn)
+                                    : null,
+                                child: Text(
+                                  locals.loginViewLoginButton,
+                                  style: defaultTheme.textTheme.bodyText1
+                                      ?.copyWith(color: Colors.white),
+                                ),
+                              ).toButton(enabled: userAuth.termsAccepted)
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          locals.loginViewForgotPasswordLink,
+                          textAlign: TextAlign.center,
+                          style: theme.textTheme.bodyText2?.copyWith(
+                            color: Colors.white,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ).toClickable(
+                            onTap: () => _openForgotPassword(context)),
+                        const SizedBox(
+                          height: Sizes.marginViewBorderSize,
+                        ),
+                        Text.rich(
+                          textAlign: TextAlign.center,
+                          TextSpan(
+                            text: locals.loginViewRegisterLinkPrefix,
+                            style: defaultTheme.textTheme.bodyText2
+                                ?.copyWith(color: Colors.white),
+                            children: [
+                              TextSpan(
+                                text: locals.loginViewRegisterLink,
+                                style: defaultTheme.textTheme.bodyText2
+                                    ?.copyWith(
+                                        color: Colors.white,
+                                        decoration: TextDecoration.underline),
+                              ),
+                            ],
+                          ),
+                        ).toClickable(onTap: () => _openRegistering(context)),
+                      ],
+                    ),
+                  ]),
             ),
-          ],
-        ).withBackground(),
-      ),
+          ),
+        ],
+      ).withBackground().withWillPopScope(context),
     );
   }
 
