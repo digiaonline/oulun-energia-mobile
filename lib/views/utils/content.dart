2ecd766b02d285fe125c5216ec2724fadb05c994
Jani Koponen <jani.koponen@digia.com>
2023-01-31 09:03:42 +0200

    Use Sizes instead of magic numbers

--- lib/views/utils/content.dart
+++ lib/views/utils/content.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:oulun_energia_mobile/views/theme/default_theme.dart';
+import 'package:oulun_energia_mobile/views/theme/sizes.dart';
+import 'package:oulun_energia_mobile/views/utils/widget_ext.dart';
 
 class Content extends StatelessWidget {
   const Content({
@@ -18,52 +20,49 @@ class Content extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        width: double.infinity,
-        color: Colors.white,
-        child: Column(
-          children: [
-            if (image != null) image!,
-            // TODO Show the image here if assigned
-            //  Container is a placeholder for the image for now
-            if (image != null)
-              Container(
-                color: Colors.grey,
-                width: double.infinity,
-                height: 235.0,
-              ),
-            Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 20),
-                    FittedBox(
-                      fit: BoxFit.fitWidth,
-                      child: Text(
-                        title,
-                        style:
-                            textTheme.headline1?.copyWith(color: Colors.black),
-                      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          if (image != null) image!,
+          // TODO Show the image here if assigned
+          //  Container is a placeholder for the image for now
+          if (image is SizedBox)
+            Container(
+              color: Colors.grey,
+              width: double.infinity,
+              height: 235.0,
+            ),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(
+                  Sizes.itemDefaultSpacing, 0.0, Sizes.itemDefaultSpacing, 0.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: Sizes.itemDefaultSpacing),
+                  FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      title,
+                      style: textTheme.headline1?.copyWith(color: Colors.black),
                     ),
-                    const SizedBox(height: 20),
-                    Text(
-                      text,
-                      style: defaultTheme.textTheme.bodyText2,
+                  ),
+                  const SizedBox(height: Sizes.itemDefaultSpacing),
+                  Text(
+                    text,
+                    style: defaultTheme.textTheme.bodyText2,
+                  ),
+                  const SizedBox(height: Sizes.itemDefaultSpacing),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: children,
                     ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: children,
-                      ),
-                    ),
-                  ],
-                ))
-          ],
-        ),
-      ),
+                  ),
+                ],
+              ))
+        ],
+      ).withBackgroundColor(Colors.white),
     );
   }
 }
