b8f1d8b62470c28493368b70b145c23383ff02eb
Jani Koponen <jani.koponen@digia.com>
2023-02-07 08:26:42 +0200

    Enable error reporting button to go to fault service view

--- lib/views/main/home_view.dart
+++ lib/views/main/home_view.dart
@@ -7,6 +7,7 @@ import 'package:oulun_energia_mobile/core/enums.dart';
 import 'package:oulun_energia_mobile/providers/login_provider.dart';
 import 'package:oulun_energia_mobile/views/contact/contact_us_view.dart';
 import 'package:oulun_energia_mobile/views/fishway/fish_way.dart';
+import 'package:oulun_energia_mobile/views/interruptions/interruptions_fault_view.dart';
 import 'package:oulun_energia_mobile/views/interruptions/interruptions_notices_view.dart';
 import 'package:oulun_energia_mobile/views/newsletter/newsletter_view.dart';
 import 'package:oulun_energia_mobile/views/theme/default_theme.dart';
@@ -139,7 +140,11 @@ List<Widget> _buildControls(
       onTap: () => context.goNamed(FishWay.routeName),
     ),
     buildHomeViewButton(
-        locals.homeViewErrorReporting, 'assets/icons/calendar.svg'),
+        locals.homeViewErrorReporting, 'assets/icons/calendar.svg',
+        onTap: () => context.goNamed(
+              InterruptionsFaultView.routeName,
+              extra: GoRouter.of(context).location,
+            )),
     buildHomeViewButton(locals.homeViewHelp, 'assets/icons/menu_book.svg'),
   ];
 }
